3ed046a88b71ec706d5f3a17fb31999e0539cdb8
Mngomz <smngomezulu@mngomz.co.za>
2023-02-16 13:23:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+/* main - main block
+ * return: 0
+ */
+
 int main(){
-	puts("Programming is like building a multilingual puzzle.\n");
+	puts("Programming is like building a multilingual puzzle.");
 	return(0);
 }
