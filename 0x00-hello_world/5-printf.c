ad7c7128c755d439c02a97e83d14bc2b6e580a2f
Mngomz <smngomezulu@mngomz.co.za>
2023-02-16 14:05:36 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,8 @@
  * main - main block
  * return: 0
  */
-
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art\n");
-        return(0);
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
 }
-
