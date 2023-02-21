faf86558a7847812558e0b88dd47a9b8e442358b
Mngomz <smngomezulu@mngomz.co.za>
2023-02-21 13:11:36 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: 1 for positive value, -1 for negative value or 0 for other
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar();
+	return (0);
+	}
+}
