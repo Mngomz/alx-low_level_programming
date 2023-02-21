a4d65f2bd4d78e8906fe8f6472cdef74d651fc4c
Mngomz <smngomezulu@mngomz.co.za>
2023-02-21 15:28:40 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add values
+ *
+ * @c1: the first number
+ *
+ * @c2: the second number
+ * 
+ * Return: Interger value
+ */
+
+int add(int c1, int c2)
+{
+	return (c1 + c2);
+}
