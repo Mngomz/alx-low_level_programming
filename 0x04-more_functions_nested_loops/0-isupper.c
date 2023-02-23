b936c1a202b1625d0e5432cfa526858905f00fca
Mngomz <smngomezulu@mngomz.co.za>
2023-02-23 11:33:30 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characher
+ *
+ * @c: tests for uppercase
+ *
+ * Return: 1 if uppercase otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 98)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	return (0);
+}
