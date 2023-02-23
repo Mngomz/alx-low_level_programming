21dda134771072f9d6f747fc1df44c112d0f1afd
Mngomz <smngomezulu@mngomz.co.za>
2023-02-23 13:50:55 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 	{
 	if ((a % 3 == 0) && (a % 5 == 0))
 	{
-	printf("Fizzbuzz");
+	printf("FizzBuzz");
 	}
 	else if (a % 3 == 0)
 	{
-	printf("fizz");
+	printf("Fizz");
 	}
 	else if (a % 5 == 0)
 	{
