592048e1328cba6737931afdf24d4a2722d6aa0a
Mngomz <smngomezulu@mngomz.co.za>
2023-02-27 18:44:12 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,13 +19,13 @@ int _atoi(char *s)
 	{
 	if (s[c] == 45)
 	{
-	main *= -1;
+	min *= -1;
 	}
 	while (s[c] >= 48 && s[c] <= 57)
 	{
 	isi = 1;
 	ni = (ni * 10) + (s[c] - '0');
-	c++
+	c++;
 	}
 	if (isi == 1)
 	{
@@ -34,5 +34,5 @@ int _atoi(char *s)
 	c++;
 	}
 	ni *= min;
-	return (oi * pn);
+	return (ni);
 }
