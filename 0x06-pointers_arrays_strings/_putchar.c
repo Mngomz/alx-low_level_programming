cb1528c2757ad1bebda7afd6b4cbefcd33ca00fe
Mngomz <smngomezulu@mngomz.co.za>
2023-03-01 13:45:33 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints character to stdout
+ *
+ * @c: the character to print
+ *
+ * Return: On success 1 and on error -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
