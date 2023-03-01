1e779b25a8830fad001cb05822cf39ca493f723e
Mngomz <smngomezulu@mngomz.co.za>
2023-03-01 14:45:20 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ *
+ * @s: the string to be changed
+ *
+ * Return: the pointer to dest
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+}
