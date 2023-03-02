204d61cca2222a703888c5422a9a0cbdbc511020
Mngomz <smngomezulu@mngomz.co.za>
2023-03-02 14:20:16 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,26 +3,26 @@
 /**
  * leet - encodes a string into 1337
  *
- * @str: input string
+ * @n: input string
  *
  * Return: the pointer to dest
  */
 
-char *leet(char *str)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTiL";
 	char s2[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 	for (j = 0; j < 10; j++)
 	{
-	if (str[i] == s1[j])
+	if (n[i] == s1[j])
 	{
-	str[i] = s2[j];
+	n[i] = s2[j];
 	}
 	}
 	}
-	return (str);
+	return (n);
 }
