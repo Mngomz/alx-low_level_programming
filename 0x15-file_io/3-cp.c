a7efeb99caa8dc022bfc30bbe00f89ff8cfdfd01
Mngomz <smngomezulu@mngomz.co.za>
2023-07-10 16:12:51 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,113 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - bytes for a buffer
+ *
+ * @file: pointer
+ *
+ * Return: a pointer
+ */
+
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't write to %s\n", file);
+	exit(99);
+	}
+
+	return (buf);
+}
+
+/**
+ * close_file - close descriptors.
+ *
+ * @fd: file descriptor
+ */
+
+void close_file(int fd)
+{
+	int s;
+
+	s = close(fd);
+
+	if (s == -1)
+	{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+	}
+}
+
+/**
+ * main - Check the code
+ *
+ * @argc: number of arguments
+ *
+ * @argv: arguments vector
+ *
+ * Return: Always 0
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+	}
+
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+	if (from == -1 || r == -1)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't read from file %s\n", argv[1]);
+	free(buffer);
+	exit(98);
+	}
+
+	w = write(to, buffer, r);
+	if (to == -1 || w == -1)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't write to %s\n", argv[2]);
+	free(buffer);
+	exit(99);
+	}
+
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}
